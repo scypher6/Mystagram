1ffc0ca6304e4eef4e1580a1b9c548ecd7d51581
scypher6 <scypher6@gmail.com>
2020-08-31 21:30:00 -0400

    documenting user edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_002430) do
+ActiveRecord::Schema.define(version: 2020_09_01_012643) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "count"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_01_03_002430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
