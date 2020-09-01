5a04176e2b7e7fdf1094bb6c19c23ebf1536e087
scypher6 <scypher6@gmail.com>
2020-08-31 23:38:47 -0400

    implemented update action for user profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
         # @user = current_user
         @user = User.find(params[:id])
     end
+
+    def update
+        @user = User.find(params[:id])
+        
+        if (@user.update(user_params))
+        else
+            render "edit"
+        end
+        byebug
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :picture)
+    end
+
 end
