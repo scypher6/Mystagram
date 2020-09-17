222c369dc8f5aa053e454a8b1a4411d9477120e4
scypher6 <scypher6@gmail.com>
2020-09-17 08:10:42 -0400

    documentation for fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
     protected
   
       def after_update_path_for(resource)
-        
+        #redirect to edit page, instead of index by default
+        # see: https://github.com/heartcombo/devise/wiki/How-To:-Customize-the-redirect-after-a-user-edits-their-profile
         edit_user_registration_path(resource)
       end
   end
