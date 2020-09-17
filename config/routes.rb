0b9c050668af5ca6205ba3da470ae8789fb9cdb3
scypher6 <scypher6@gmail.com>
2020-09-17 08:02:50 -0400

    route to new overriding registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :likes
-
+  devise_for :users, :controllers => { :registrations => :registrations }
   devise_scope :user do
     authenticated :user do
       root to: 'pics#index'
