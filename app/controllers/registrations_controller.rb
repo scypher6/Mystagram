f6c3d1717aee93f7b60e3b44f0a5d01ef07ed7b3
scypher6 <scypher6@gmail.com>
2020-09-17 08:07:03 -0400

    fixed issue where user would be sent to index page after editing profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
     protected
   
       def after_update_path_for(resource)
-        byebug
-        user_path(resource)
+        
+        edit_user_registration_path(resource)
       end
   end
